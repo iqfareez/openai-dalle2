771a8ec5d02fd2afa4b114407608ac26d694e952
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-02-26 12:49:11 +0000

    :safety_vest: Added input prompt

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ load_dotenv()  # take environment variables from .env.
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+input_prompt = input("Enter your image prompt: ")
+
+# If input is empty string, exit the program
+if input_prompt == "":
+    print("No input given...")
+    exit()
+
 res = openai.Image.create(
-    prompt="A depiction of life in year 3040",
-    n=1,  # number of output images
+    prompt=input_prompt,
+    n=2,  # number of output images
     size="512x512")
 
 image_urls = res["data"]
